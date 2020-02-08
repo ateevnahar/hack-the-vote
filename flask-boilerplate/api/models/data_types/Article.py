e00d83de531f76d733e2962f8fdab04f9cfc30cb
Nicholas Mullikin <nicholas.mullikin@gmail.com>
2020-02-08 18:57:18 -0500

    more data

--- flask-boilerplate/api/models/data_types/Article.py
+++ flask-boilerplate/api/models/data_types/Article.py
@@ -1,7 +1,8 @@
 class Article:
-    def __init__(self, url, topics):
+    def __init__(self, url, topics, summary):
         self.url = url
         self.topics = topics
+        self.summary = summary
 
     def add_issue_topic(self, topics):
         self.topics.append(topics)
